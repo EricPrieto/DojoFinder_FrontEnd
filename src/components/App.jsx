3e004d3f3b435071f107d43a593af8c6551860d0
EricPrieto <ep1043@hotmail.com>
2021-12-10 14:08:41 -0600

    Added components header and footer

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import react, { Component } from 'react';
+// import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import jwt_decode from 'jwt-decode';
+// import axios from 'axios';
+
+import Header from './Header';
+import HomeGuest from './HomeGuest';
+import Footer from './Footer';
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+
+function ExampleComponent(){
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
+  )
+}
+
+
+export default App;
